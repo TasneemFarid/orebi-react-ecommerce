43d3084f1e6a2af909ff617329e8dfcf0e2f990b
Tasneem Farid <tasneemfarid.webdev@gmail.com>
2023-11-29 21:33:16 +0600

    cart sidebar

--- src/components/layouts/Searchbar.jsx
+++ src/components/layouts/Searchbar.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 import { BiSolidUser } from "react-icons/bi";
 import { BsSearch } from "react-icons/bs";
 import { FaShoppingCart } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { pagename } from "../../slices/breadcrumbSlices";
+import { decrement, increment, removeFromCart } from "../../slices/cartSlices";
 import Container from "../Container";
 import Flex from "../Flex";
 
@@ -16,8 +18,23 @@ const Searchbar = () => {
   let handleBreadCrumb = (name) => {
     dispatch(pagename(name));
   };
-  
   let cart = useSelector((state) => state.cart.cartitem);
+  let handleIncrement = (item) => {
+    dispatch(increment(item));
+  };
+  let handleDecrement = (item) => {
+    dispatch(decrement(item));
+  };
+
+  let [total, setTotal] = useState(0);
+  useEffect(() => {
+    let total = 0;
+    cart.map((item) => {
+      total += item.price * item.quantity;
+    });
+    setTotal(total);
+  }, [cart]);
+
   return (
     <section className="py-6 mb-32 bg-ash">
       <Container>
@@ -46,13 +63,47 @@ const Searchbar = () => {
           </Flex>
         </Flex>
         {open && (
-          <div className="w-2/6 bg-ash h-screen absolute top-0 right-0 z-10">
+          <div className="w-2/6 bg-gray-700 h-screen absolute top-0 right-0 z-10 p-5">
             <FaShoppingCart onClick={() => setOpen(false)} />
-            <ul>
-              {cart.map(item=>(
-                <li>{item.title} {item.price} - {item.quantity}</li>
-              ))}
+            <ul className="flex justify-between bg-ash py-5 px-2">
+              <li>Product</li>
+              <li>Price</li>
+              <li>Quantity</li>
+              <li>Subtotal</li>
+              <li>Action</li>
             </ul>
+            {cart.length > 0 ? (
+              cart.map((item) => (
+                <ul className="flex justify-between items-center text-white py-5 px-2 border-t border-solid border-white">
+                  <li>{item.title}</li>
+                  <li>{item.price}</li>
+                  <li className="border border-solid border-white py-1 px-5">
+                    <button
+                      className="mr-2"
+                      onClick={() => handleDecrement(item)}
+                    >
+                      -
+                    </button>
+                    {item.quantity}
+                    <button
+                      className="ml-2"
+                      onClick={() => handleIncrement(item)}
+                    >
+                      +
+                    </button>
+                  </li>
+                  <li>{item.price * item.quantity}</li>
+                  <li onClick={() => dispatch(removeFromCart(item))}>
+                    <IoMdClose className="text-2xl" />
+                  </li>
+                </ul>
+              ))
+            ) : (
+              <h3 className="text-white text-center pt-5">Cart is Empty</h3>
+            )}
+            <h3 className="absolute bottom-5 right-5 text-white">
+              Total: {total}
+            </h3>
           </div>
         )}
       </Container>
--- src/slices/cartSlices.js
+++ src/slices/cartSlices.js
@@ -22,10 +22,42 @@ export const cartSlices = createSlice({
         state.cartitem.push(action.payload);
       }
     },
+    increment: (state, action) => {
+      state.cartitem.map((item) => {
+        if (item.title == action.payload.title) {
+          item.quantity = item.quantity + 1;
+        }
+      });
+    },
+    decrement: (state, action) => {
+      state.cartitem.map((item, index) => {
+        if (item.title == action.payload.title) {
+          if (item.quantity > 1) {
+            item.quantity = item.quantity - 1;
+          } else {
+            state.cartitem.splice(index, 1);
+          }
+        }
+      });
+    },
+    removeFromCart: (state, action) => {
+      // let index;
+      // index = state.cartitem.findIndex(
+      //   (item) => item.title == action.payload.title
+      // );
+      // state.cartitem.splice(index, 1);
+
+      state.cartitem.map((item, index) => {
+        if (item.title == action.payload.title) {
+          state.cartitem.splice(index, 1);
+        }
+      });
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addtocart } = cartSlices.actions;
+export const { addtocart, increment, decrement, removeFromCart } =
+  cartSlices.actions;
 
 export default cartSlices.reducer;
